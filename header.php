d560772c586573262a0d5de8911d5a41e894df13
Chaitali Shah <chaitalishah@Chaitali-ka-MacBook-Air.local>
2026-07-20 14:58:04 +0530

    Header updated

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying the header
+ * The template for displaying the header - through github
  *
  * This is the template that displays all of the <head> section, opens the <body> tag and adds the site's header.
  *
